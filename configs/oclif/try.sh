8bdd7c2d6a65f356eaa7e8bc4de6388f6ff32de3
Piero Proietti <piero.proietti@gmail.com>
2023-02-03 17:53:31 +0100

    can't work!!!

--- configs/oclif/try.sh
+++ configs/oclif/try.sh
@@ -1,39 +1,107 @@
-#!/usr/bin/env bash
-MSP=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $MSP
-source ${MSP}/includes/easybashgui
-source ${MSP}/includes/easybashgui.lib
-source ${MSP}/includes/common.sh
+#!/usr/bin/env bash 
+function main() {
+    config
 
-function query() {
-    # working 
-    # - echo ${root} | jq ".commands[] | select(.id=="'"calamares"'") | .flags[].name"
-    # - echo ${root} | jq "${filter}"
-    # - result=`echo ${root} | jq "${filter}"`
-    #
-    result=`echo ${root} | jq "${filter}"`
+    # root    
+    root=$(cat .oclif.manifest.json | jq .)
+
+    # [jq]>  .commands[] | select(.id | contains(":")).id   
+    #filter=".commands[] | select(.id | contains(\":\")).id"
+
+    #id="dad"
+    #filter=".commands[] | select(.id==\"${id}\").id"
+
+    filter=".commands[].id"
+    result=$(query)
+    result+=("quit")
+
+
+    while true; do
+        clear
+        # NOTE [@]
+        menu ${result[@]}
+        answer=$(0< "${dir_tmp}/${file_tmp}" )
+
+        case "$answer" in 
+            adapt)
+                ;;
+            analyze)
+                ;;
+            calamares)
+                ;;
+            config)
+                ;;
+            cuckoo)
+                ;;
+            dad)
+                ;;
+            install)
+                ;;
+            kill)
+                ;;
+            mom)
+                ;;
+            produce)
+                ;;
+            status)
+                ;;
+            syncfrom)
+                ;;
+            syncto)
+                ;;
+            update) 
+                ;;
+            export) # submenu
+                submenu ${answer};;
+            tools) # submenu
+                submenu ${answer};;
+            wardrobe) # submenu
+                submenu ${answer};;
+            *) # quit and others
+                theEnd
+                ;;
+        esac
+    done
+
+    exit 0
 }
 
-function initMenu() {
+# 
+# filter=".commands[] | select(.id | contains(\":\")).id"
+# filter=".commands[] | select(.id==\"${id}\").id"
+function query() {
+    local retval=$(echo ${root} | jq "${filter}")
+    echo ${retval}    
+}
+
+################################
+function config() {
+    # configurations
+    set -e
+
     # Disable unicode.
     LC_ALL=C
     LANG=C
 
-    # working 
-    # - root=$(cat .oclif.manifest.json | jq .)
-    # - filter=".commands[] | select(.id=="'"calamares"'") | .flags[].name"
-    # - filter=".commands[] | select(.id==\"${id}\") | .flags[].name"
-    # - query "${filter}"
-    root=$(cat .oclif.manifest.json | jq .)
-  
+    # easy-bash-gui
+    MSP=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    source ${MSP}/includes/easybashgui
+    source ${MSP}/includes/easybashgui.lib
+    source ${MSP}/includes/common.sh
+
 }
 
-initMenu
+################################
+function press_a_key_to_continue {
+   read -rp "Press enter to continue"
+}
 
-id="dad"
-filter=".commands[] | select(.id==\"${id}\") | .flags[].name"
-query "${filter}"
-echo "filter: ${filter}"
-echo "result: ${result}"
-exit 0
+################################
+function theEnd {
+   echo "regular exit"
+   exit 0
+}
 
+main
+
+# EOF()
