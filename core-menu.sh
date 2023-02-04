c7141c0eab5fc325b8ae6f12a4aa5a438670d387
Jon West <electrikjesus@gmail.com>
2023-02-03 20:22:20 -0500

    [core] Rework menu system
    - Switch to using functions for menu 
    - Add Previous/Current menu trackers (still buggy, needs more levels)

--- core-menu.sh
+++ core-menu.sh
@@ -82,6 +82,302 @@ export supertitle="Start Menu"
 export supericon=""
 source $INCLDIR/common.sh
 
+function goto
+{
+    label=$1
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+menuParse() {
+  # Root Menu:
+  # (jq -r '.options.menuEntry[]? | .name' $options_path)
+  # First Child Menu:
+  # (jq -r '.options.menuEntry[]? | select(.name == "this") | .subMenuEntry[]? | .name' $options_path)
+  # First Child Dependency:
+  # (jq -r '.options.menuEntry[]? | select(.name == "this") | .dependencies[]? | .dep' $options_path)
+
+  # We will keep the base address static
+  BASE_ENTRY=".options"
+
+  # Define MENUENTRY if not passed ($1)
+  if [ ! "$1" ]; then
+    MENUENTRY=".menuEntry[]?"
+  else
+    MENUENTRY="$1"
+  fi
+  # Define THIS_MENU if not passed ($2)
+  if [ ! "$2" ]; then
+    THIS_MENU=".name"
+  else
+    THIS_MENU="$2"
+  fi
+  LAST_MENUENTRY="$MENUENTRY"
+  LAST_MENUNAME="$THIS_MENU"
+
+  current_menuentry="$MENUENTRY"
+  current_menu="$THIS_MENU"
+  
+  if [[ $current_menuentry != $previous_menuentry ]] || [[ $current_menu != $previous_menu ]]; then
+    if [[ -n "$previous_menu" ]]; then
+      echo "New menu: $previous_menu"
+    fi
+    echo -e ""
+    echo -e "Menu ${BLUE}$current_project${NC}"
+    echo -e ""
+  else
+    current_menuentry="$previous_menuentry"
+    current_menu="$previous_menu"
+    MENUENTRY="$previous_menuentry"
+    THIS_MENU="$previous_menu"
+  fi
+
+  # If a third and fourth variables are passed, we use those for a search definition
+  # $3 = variable_name
+  # $4 = Variable_Value
+  if [[ "$3" ]] && [[ "$4" ]]; then
+    THIS_SEARCH="select($3 == '"$4"')"
+  else
+    THIS_SEARCH=""
+  fi
+  if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+    echo "MENUENTRY: $MENUENTRY"
+    echo "THIS_MENU: $THIS_MENU"
+    echo "THIS_SEARCH: $THIS_SEARCH"
+  fi
+  parseMenu(){
+    # Parse menu
+    MENUOPTIONS=()
+    while IFS= read -r entry; do
+      if [[ "$EZ_DEBUG_MODE" == "true" ]]; then 
+        echo "Parsing Menuoptions Entry: $entry"
+      fi
+      # Check Dependency Here First
+      MENUITEMDEPS=()
+      while IFS= read -r menuitem; do
+        if [[ "$menuitem" != "" ]]; then
+          MENUITEMDEPS+=("$menuitem")
+          $menuitem # 2>/dev/null;
+          if [ $? -eq 0 ]; then
+            if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+              echo "Dependency Passed: $menuitem"
+            fi
+          else
+            if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+              echo "Dependency Falied: $menuitem"
+            fi
+            smconfigerror="true";
+          fi
+        fi
+      done < <(jq -r "$BASE_ENTRY$MENUENTRY | select(.name == \"${entry}\") | .dependencies[]?.dep" $options_path)
+      
+      # If dependency passes, add entry
+      if [[ ! "$smconfigerror" ]]; then
+        MENUOPTIONS+=("$entry")
+      fi
+
+    done < <(jq -r "$BASE_ENTRY$MENUENTRY$THIS_MENU" $options_path)
+
+  }
+  parseMenu
+  while :
+    do
+
+    if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+      echo "MENUENTRY: $MENUENTRY"
+      echo "THIS_MENU: $THIS_MENU"
+      echo "previous_menuentry: $previous_menuentry"
+      echo "previous_menu: $previous_menu"
+      echo "current_menuentry: $current_menuentry"
+      echo "current_menu: $current_menu"
+    fi
+    if [[ "$SUBMENUENTRY" == "" ]] && ([[ "$previous_menuentry" != "" ]] || \
+    [[ "$previous_menu" != "" ]]) && ([[ "$previous_menuentry" != "$current_menuentry" ]] || \
+    [[ "$previous_menu" != "$current_menu" ]]); then
+      echo "NEW SUBMENU"
+      MENUENTRY="$previous_menuentry"
+      THIS_MENU="$previous_menu"
+      if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+        echo "MENUENTRY: $MENUENTRY"
+        echo "THIS_MENU: $THIS_MENU"
+      fi
+      parseMenu
+    fi
+
+    menu ".." "${MENUOPTIONS[@]}"
+    answer=$(0< "${dir_tmp}/${file_tmp}")
+    
+    for i in "${MENUOPTIONS[@]}" ; do
+      i="$(echo ${i} |  tr -d '\n' | tr -d '\r' | tr -d \")"
+      if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "${i}" ]]; then
+        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+          echo "Starting \"${i}\" ..."
+        fi
+        # Check for Flags here, to see if we need to form any options
+
+        # If Flags, and Command are both present, we find n flags.
+        # Command
+        # COMMAND=()
+        # while IFS= read -r com; do
+        #   COMMAND+=("$com")        
+        # done < <(jq -r "$BASE_ENTRY$MENUENTRY | select(.name == \"${i}\") | .command" $options_path)
+        COMMAND="$(jq -r "$BASE_ENTRY$MENUENTRY | select(.name == \"${i}\") | .command" $options_path)"
+        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+          echo "Command: $COMMAND"
+        fi
+        # Flags
+        FLAGS=()
+        while IFS= read -r flag; do
+          FLAGS+=("$flag")
+        done < <(jq -r "$BASE_ENTRY$MENUENTRY | select(.name == \"${i}\") | .flags[]?.flag" $options_path)
+        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+          echo "FLAGS: ${FLAGS[@]}"
+        fi
+        if [[ "${FLAGS[@]}" != "" ]]; then
+          FLAG_ANSWERS=""
+          menu ".." "${FLAGS[@]}"
+          answer=$(0< "${dir_tmp}/${file_tmp}")
+          for f in "${FLAGS[@]}" ; do
+            if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "${f}" ]]; then
+              flag_name="$f"
+              flag_description="$(jq -r "$BASE_ENTRY$MENUENTRY | select(.name == \"${i}\") | .flags[]? | select(.flag == \"${f}\") | .descr" $options_path)"
+              flag_type="$(jq -r "$BASE_ENTRY$MENUENTRY | select(.name == \"${i}\") | .flags[]? | select(.flag == \"${f}\") | .type" $options_path)"
+
+              if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+                echo "flag_name: ${flag_name}"
+                echo "flag_description: ${flag_description}"
+                echo "flag_type: ${flag_type}"
+              fi
+              
+              if [[ "$flag_type" == "boolean" ]]; then
+                # Show popup question for bool
+                question "$flag_description"
+                flag_answer="${?}"
+                if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+                  echo "flag_answer: ${flag_answer}"
+                fi
+                if [[ "${flag_answer}" = 0 ]]; then
+                  flag_command="$flag_name"
+                elif [[ "${flag_answer}" == "1" ]]; then
+                  flag_command=""
+                fi
+              elif [[ "$flag_type" == "string" ]]; then
+                # Show popup question for bool
+                input "$flag_description"
+                flag_answer=$(0< "${dir_tmp}/${file_tmp}" )
+                if {[ ${flag_answer} != "" ]}; then
+                  flag_command="$flag_name $flag_answer"
+                fi
+              fi
+              FLAG_ANSWERS="${FLAG_ANSWERS} $flag_command"
+              if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+                echo "flag_command: ${flag_command}"
+                echo "FLAG_ANSWERS: ${FLAG_ANSWERS}"
+              fi
+            fi
+
+            if [[ "${answer}" == ".." ]]; then
+              export SUBMENUENTRY=""
+              export THIS_SUBMENU=""
+              export MENUENTRY=""
+              export THIS_MENU=""
+              if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+                echo "Going Back"
+              fi
+              previous_menuentry="$current_menuentry"
+              previous_menu="$current_menu"
+              continue 2
+            fi
+
+            if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "" ]]; then
+              if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+                echo "exiting..."
+              fi
+              [[ $_ != $0 ]] && exit 0 2>/dev/null || return 0 2>/dev/null;
+            fi
+
+          done
+          # we parse the command
+          PARSED_MENU_OPTION_COMMAND="$COMMANDS $FLAG_ANSWERS"
+          if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+            echo "Parsed flag command: $PARSED_MENU_OPTION_COMMAND"
+          fi
+        else 
+          # we parse the command
+          PARSED_MENU_OPTION_COMMAND="$COMMANDS"
+          if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+            echo "Parsed command: $PARSED_MENU_OPTION_COMMAND"
+          fi
+        fi
+        if [[ "$PARSED_MENU_OPTION_COMMAND" != "" ]]; then
+          $PARSED_MENU_OPTION_COMMAND
+        fi
+        # Do submenu operation
+
+        SUBMENUOPTIONS=()
+        while IFS= read -r option; do
+          SUBMENUOPTIONS+=("$option")
+        done < <(jq -r "$BASE_ENTRY$MENUENTRY | select(.name == \"${i}\") | .menuEntry[]? | .name" $options_path)
+        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+          echo "SUBMENUOPTIONS: ${SUBMENUOPTIONS[@]}"
+        fi
+        if [[ "${SUBMENUOPTIONS[@]}" != "" ]]; then
+          export SUBMENUENTRY="$MENUENTRY | select(.name == \"${i}\") | .menuEntry[]?"
+          export THIS_SUBMENU=".name"
+          if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+            echo "SUBMENUENTRY: $SUBMENUENTRY"
+            echo "THIS_SUBMENU: $THIS_SUBMENU"
+          fi
+          # LAST_MENUENTRY="$MENUENTRY"
+          # LAST_MENUNAME="$THIS_MENU"
+          previous_menuentry="$current_menuentry"
+          previous_menu="$current_menu"
+          return 0
+        else
+          export SUBMENUENTRY=""
+          export THIS_SUBMENU=""
+          export MENUENTRY=""
+          export THIS_MENU=""
+          if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+            echo "SUBMENUENTRY: $SUBMENUENTRY"
+            echo "THIS_SUBMENU: $THIS_SUBMENU"
+          fi
+          # goto "$menu"
+          return 0
+        fi
+      fi
+
+      if [[ "${answer}" == ".." ]]; then
+        export SUBMENUENTRY=""
+        export THIS_SUBMENU=""
+        export MENUENTRY=""
+        export THIS_MENU=""
+        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+          echo "Clearing vars and moving back to parent menu"
+        fi
+        break && break
+      fi
+
+      if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "" ]]; then
+        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+          echo "exiting..."
+        fi
+        [[ $_ != $0 ]] && exit 0 2>/dev/null || return 0 2>/dev/null;
+      fi
+      
+    done
+    if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+      echo "Exiting MENUOPTIONS for loop"
+    fi
+  done
+  if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+    echo "Exiting menuParse()"
+  fi
+
+}
+
 # if debug or tty, set "supermode" var to "none"
 # export supermode="none" && source easybashgui
 if [[ "$EASYLEGACY" == "true" ]]; then
@@ -121,175 +417,32 @@ fi
 #   - command (string)
 #   - dependencies (array)
 
-MENUOPTIONS=()
-while IFS= read -r entry; do
-    MENUOPTIONS+=("$entry")
-    if [[ "$EZ_DEBUG_MODE" == "true" ]]; then 
-      echo "Menuoptions Entry: $entry"
-    fi
-done < <(jq -r '.options.menuEntry[]?.name' $options_path)
+# Do root level menu
+if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+  echo
+  echo "Running First Menu"
+  echo
+fi
 
-while :
-	do
-  menu "${MENUOPTIONS[@]}"
-	answer=$(0< "${dir_tmp}/${file_tmp}")
-	
-	for i in "${MENUOPTIONS[@]}" ; do
-    i="$(echo ${i} |  tr -d '\n' | tr -d '\r' | tr -d \")"
-		if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "${i}" ]]; then
-      if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-			  echo "Starting \"${i}\" ..."
-      fi
-			j=${i}
-      if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-        echo -e ${reset}""${reset}
-        echo -e ${teal}"${i}"${reset}
-        echo -e ${reset}""${reset}
-      fi
-      SUBMENUOPTIONS=()
-      while IFS= read -r subentry; do
-          SUBMENUOPTIONS+=("$subentry")
-      done < <(jq -r '.options.menuEntry[]? | select(.name == "'"$i"'") | .subMenuEntry[]?.name' $options_path)
-      if [[ "${SUBMENUOPTIONS[@]}" != "" ]]; then
-        while :
-        do
-          menu ".." "${SUBMENUOPTIONS[@]}"
-          answer=$(0< "${dir_tmp}/${file_tmp}" )
-          
-          for s in "${SUBMENUOPTIONS[@]}" ; do
-            s="$(echo ${s} |  tr -d '\n' | tr -d '\r' | tr -d \")"
-            if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "${s}" ]]; then
-              notify_message "Starting \"${s}\" ..."
-              if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-                echo -e ${reset}""${reset}
-                echo -e ${ltgreen}"${s}"${reset}
-                echo -e ${reset}""${reset}
-              fi
-              # Check Dependencies
+# Create a variable to jump to:
+# menu=${1:-"menu"}
+# goto "$menu"
 
-              SUBMENUITEMDEPS=()
-              while IFS= read -r menuitem; do
-                  SUBMENUITEMDEPS+=("$menuitem")
-              done < <(jq -r '.options.menuEntry[]? | select(.name == "'"$i"'") | .dependencies[]?.dep' $options_path)
-              smconfigerror="false"
-              if [ "${SUBMENUITEMDEPS[@]}" ]; then
-                for sm in "${SUBMENUITEMDEPS[@]}" ; do
-                  $sm # 2>/dev/null;
-                  if [ $? -eq 0 ]; then
-                    if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-                      echo OK
-                    fi
-                  else
-                    if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-                      echo FAIL
-                    fi
-                    smconfigerror="true";
-                  fi
-                done
-              fi
-              if [[ "$smconfigerror" != "true" ]]; then
-                # Execute Option
-                if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-                  echo "executing command for: ${s}"
-                fi
-                execSubCommand=()
-                while IFS= read -r subentry; do
-                    execSubCommand+=("$subentry")
-                done < <(jq -r '.options.menuEntry[]? | select(.name == "'"$i"'") | .subMenuEntry[]? | select(.name == "'"$s"'") | .command' $options_path)
-                if [[ "$EZ_DEBUG_MODE" == "true" ]]; then 
-                  echo "Ececuting SubCommand Entry: $execSubCommand"
-                fi
-                $execSubCommand # 2>/dev/null;
-                if [ $? -eq 0 ]; then
-                  if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-                    echo OK
-                  fi
-                else
-                  if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-                    echo "The script failed" >&2
-                  fi
-                fi
-              else
-                if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-                  echo "Dependencies not met for: $s"
-                fi
-              fi
-              continue 2
-            fi
-            
-          done
-          if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "" ]]; then
-            continue 2
-          fi
-          if [[ "${answer}" == ".." ]]; then
-            continue 2
-          fi
-        done
-      fi
+# : menu
 
-      # Check Dependencies
+menuParse
 
-      MENUITEMDEPS=()
-      while IFS= read -r menuitem; do
-          MENUITEMDEPS+=("$menuitem")
-      done < <(jq -r '.options.menuEntry[]? | select(.name == "'"$i"'") | .dependencies[]?.dep' $options_path)
-      mconfigerror="false"
-      if [ "${MENUITEMDEPS[@]}" ]; then
-        for m in "${MENUITEMDEPS[@]}" ; do
-          if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-            echo $m
-          fi
-          $m # 2>/dev/null;
-          if [ $? -eq 0 ]; then
-            if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-              echo OK
-            fi
-          else
-            if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-              echo FAIL
-            fi
-             mconfigerror="true";
-          fi
-        done
-      fi
-      if [[ "$mconfigerror" != "true" ]]; then
-        # Execute Option
-        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-          echo "executing command for: ${i}"
-        fi      
-        execCommand=()
-        while IFS= read -r cmdentry; do
-            execCommand+=("$cmdentry")
-        done < <(jq -r '.options.menuEntry[]? | select(.name == "'"$i"'") | .command' $options_path)
-        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then 
-          echo "Executing Command Entry: $execCommand"
-        fi
-        $execCommand # 2>/dev/null;
-        if [ $? -eq 0 ]; then
-          if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-            echo OK
-          fi
-        else
-          if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-            echo "The script failed" >&2;
-          fi
-        fi
-      else
-        if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-          echo "Dependencies not met for: $i"
-        fi
-      fi
+# SUBMENUENTRY=""
+# THIS_SUBMENU=""
 
-		fi
-		
-	done
-	if [[ "$(echo ${answer} |  tr -d '\n' | tr -d '\r')" == "" ]]; then
-    if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
-		  echo "exiting..."
-    fi
-    [[ $_ != $0 ]] && exit 0 2>/dev/null || return 0 2>/dev/null;
-	fi
-	
-done
+# : submenu
+if [[ "$SUBMENUENTRY" != "" ]]; then
+  if [[ "$EZ_DEBUG_MODE" == "true" ]]; then
+    echo
+    echo "Running second Menu"
+    echo
+  fi
+  menuParse "$SUBMENUENTRY" "$THIS_SUBMENU"
+fi
 
 clean_temp #(since v.6.X.X this function is no more required if easybashlib is present and then successfully loaded by easybashgui)
